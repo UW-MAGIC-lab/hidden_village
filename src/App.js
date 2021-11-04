6eee62595e30e040709bed6f8e849061f656644a
Ariel Fogel <fogeltine@gmail.com>
2021-11-04 17:37:09 -0500

    Add barebones settings component to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { StrictMode, lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Loader from "./components/utilities/Loader.js";
+import Settings from "./components/Settings.js";
 
 const Story = lazy(() => import("./Story"));
 
@@ -11,7 +12,7 @@ const App = () => {
       <Router>
         <Switch>
           <Route path="/settings">
-            <Story />
+            <Settings />
           </Route>
           <Route path="/admin">
             <Story />
---
+++ src/components/Settings.js
@@ -0,0 +1,5 @@
+const Settings = () => {
+  return <div>You made it to the setting page!</div>;
+};
+
+export default Settings;
