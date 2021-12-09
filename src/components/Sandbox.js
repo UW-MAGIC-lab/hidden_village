62812c23cfea1e929da5e75c3b284f11fff34175
Ariel Fogel <fogeltine@gmail.com>
2021-12-09 16:00:59 -0600

    update Sandbox component to have poseData

--- src/components/Sandbox.js
+++ src/components/Sandbox.js
@@ -1,10 +1,48 @@
 import { Stage } from "@inlet/react-pixi";
 import Background from "./Background";
-import { useState } from "react";
+import { useState, useEffect} from "react";
+import { Camera } from "@mediapipe/camera_utils";
+import { Holistic } from "@mediapipe/holistic/holistic";
+import { enrichLandmarks } from "./Pose/landmark_utilities";
 
 const Sandbox = () => {
+  let holistic;
   const [height, setHeight] = useState(window.innerHeight);
   const [width, setWidth] = useState(window.innerWidth);
+  const [poseData, setPoseData] = useState({});
+
+  useEffect(() => {
+    holistic = new Holistic({
+      locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
+      },
+    });
+    holistic.setOptions({
+      modelComplexity: 1,
+      smoothLandmarks: true,
+      enableSegmentation: true,
+      smoothSegmentation: true,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5,
+      selfieMode: true,
+    });
+    async function poseDetectionFrame() {
+      await holistic.send({ image: videoElement });
+    }
+
+    const videoElement = document.getElementsByClassName("input-video")[0];
+    let camera = new Camera(videoElement, {
+      onFrame: poseDetectionFrame,
+      width: window.innerWidth,
+      height: window.innerHeight,
+      facingMode: "environment",
+    });
+    camera.start();
+    const updatePoseResults = (newResults) => {
+      setPoseData(enrichLandmarks(newResults));
+    };
+    holistic.onResults(updatePoseResults);
+  }, []);
 
   return (
     <Stage
