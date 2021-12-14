50d42306c506c5686a77488095fa9a73921170bc
Ariel Fogel <fogeltine@gmail.com>
2021-12-13 18:00:40 -0600

    Draw character components (#23)
    * temporarily fix the hands do not disappear bug by using the hook useState instead without memoizing the poseData.
    
    * draw character face prototype
    
    * temporarily fix the hands do not disappear bug by using the hook useState instead without memoizing the poseData.
    
    * draw character face prototype
    
    * update so that face/character are more dynamic
    
    * update Sandbox to reflect full-featured sandbox
    
    * update Character and Face API to support all the characters we want to draw
    
    * update with final few fixes to Face/Character components
    
    * Add basic better-docs. Team, let's fill this up!
    
    * Revert "update with final few fixes to Face/Character components"
    
    This reverts commit 8e1c5b6ca0412d68f003354e6d780a3b70d5144e.
    
    * Revert "Revert "update with final few fixes to Face/Character components""
    
    This reverts commit 88d422458bea9b542886f39616b0cffbaf8af295.
    
    * Revert "Add basic better-docs. Team, let's fill this up!"
    
    This reverts commit 8af8b8c6ee4475fa92c643e366b99d8362172bf5.
    
    * add JSDOC config
    
    Co-authored-by: tyrrr110 <tren25@wisc.edu>

---
+++ src/components/Character.js
@@ -0,0 +1,154 @@
+import { Graphics, Container } from "@inlet/react-pixi";
+import { blue, pink, green } from "../utils/colors";
+import { useCallback } from "react";
+import Face from "./Face";
+import PropTypes from "prop-types";
+
+const drawCircle = (g, height) => {
+  g.drawCircle(0, 0, height);
+  g.endFill();
+};
+
+const equilateralTriangle = (g, height) => {
+  g.moveTo(height * 2, height);
+  g.lineTo(height, -height);
+  g.lineTo(0, height);
+  g.lineTo(height, height);
+  g.endFill();
+};
+
+const trapezoid = (g, height) => {
+  g.moveTo(0, height);
+  g.lineTo(height * 0.5, 0);
+  g.lineTo(1.75 * height, 0);
+  g.lineTo(2.25 * height, height);
+  g.lineTo(0, height);
+  g.endFill();
+};
+
+const square = (g, height) => {
+  g.drawRect(0, 0, height, height);
+  g.endFill();
+};
+
+const rectangle = (g, height) => {
+  g.drawRect(0, 0, height, height * 2.5);
+  g.endFill();
+};
+
+const scaleneTriangle = (g, height) => {
+  const width = height * 0.5;
+  g.moveTo(0, height);
+  g.lineTo(width / 2, 0);
+  g.lineTo(width, height);
+  g.lineTo(0, height);
+  g.endFill();
+};
+
+const moodToProps = (mood) => {
+  switch (mood) {
+    case "happy":
+      return {
+        mouthType: "happy",
+        browType: "angry",
+      };
+    case "sad":
+      return {
+        mouthType: "sad",
+        browType: "angry",
+      };
+    case "angry":
+      return {
+        mouthType: "angry",
+        browType: "angry",
+      };
+    case "neutral":
+      return {
+        mouthType: "neutral",
+        browType: "neutral",
+      };
+    case "surprised":
+      return {
+        mouthType: "surprised",
+        browType: "neutral",
+      };
+    default:
+      return {
+        mouthType: "neutral",
+        browType: "neutral",
+      };
+  }
+};
+
+/**
+ * @component
+ * @example
+ * return (
+ *   <Character
+ *    type={"rectangle"}
+ *    height={200}
+ *    placement={[200, 200]}
+ *    color={blue}
+ *    mood={"neutral"}
+ *    facePosition={[70, 50]}
+ *  />
+ * )
+ * @description
+ * type: String; One of ["circle", "equilateralTriangle", "scaleneTriangle", "square", "trapezoid", "rectangle"]
+ *
+ * color: String (hex); One of [blue, pink, green]
+ *
+ * height: Number (in pixels); often a scalar value of the height or other segment length
+ *
+ * facePosition: Array<Number>; An array with an x and y value, with (0,0) being the top left corner of the character
+ *
+ * mood: String; One of ["happy", "sad", "angry", "neutral", "surprised"]
+ *
+ * placement: Array<Number>; An array with an x and y value, with (0,0) being the top left corner of the screen
+ *
+ */
+
+const Character = (props) => {
+  const { placement, type, color, height, facePosition } = props;
+  const draw = useCallback((g) => {
+    g.clear();
+    g.beginFill(color);
+    switch (type) {
+      case "circle":
+        drawCircle(g, height);
+        break;
+      case "equilateralTriangle":
+        equilateralTriangle(g, height);
+        break;
+      case "scaleneTriangle":
+        scaleneTriangle(g, height);
+        break;
+      case "square":
+        square(g, height);
+        break;
+      case "trapezoid":
+        trapezoid(g, height);
+        break;
+      case "rectangle":
+        rectangle(g, height);
+        break;
+      default:
+        break;
+    }
+  }, []);
+
+  return (
+    <Container position={placement} scale={1}>
+      <Graphics draw={draw} />
+      <Face
+        placement={facePosition}
+        height={76}
+        width={76}
+        {...moodToProps(props.mood)}
+        eyeSpacing={type === "scaleneTriangle" ? "small" : "medium"}
+      />
+    </Container>
+  );
+};
+
+export default Character;
--- src/components/Sandbox.js
+++ src/components/Sandbox.js
@@ -1,11 +1,10 @@
 import { Stage } from "@inlet/react-pixi";
 import Background from "./Background";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { Camera } from "@mediapipe/camera_utils";
 import { Holistic } from "@mediapipe/holistic/holistic";
 import { enrichLandmarks } from "./Pose/landmark_utilities";
 import { generateRowAndColumnFunctions } from "./utilities/layoutFunction";
-
 const [
   numRows,
   numColumns,
@@ -14,6 +13,9 @@ const [
   columnGutter,
   rowGutter,
 ] = [2, 3, 20, 20, 30, 30];
+
+import Character from "./Character";
+import { yellow, blue, white, darkGray, green, pink } from "../utils/colors";
 
 const Sandbox = () => {
   let holistic;
@@ -30,6 +32,8 @@ const Sandbox = () => {
     columnGutter,
     rowGutter
   );
+  const [col1, setCol1] = useState(columnDimensions(1));
+  const [row1, setRow1] = useState(rowDimensions(1));
 
   useEffect(() => {
     holistic = new Holistic({
@@ -75,6 +79,54 @@ const Sandbox = () => {
       }}
     >
       <Background height={height} width={width} />
+      <Character
+        placement={[row1.x + row1.width * 0.4, row1.y + row1.height * 0.4]}
+        type={"circle"}
+        color={pink}
+        facePosition={[-20, -30]}
+        height={col1.height * 0.1}
+        mood={"happy"}
+      />
+      <Character
+        type={"equilateralTriangle"}
+        height={col1.height * 0.1}
+        placement={[col1.x + col1.width * 0.66, col1.y + col1.height * 0.9]}
+        color={blue}
+        mood={"happy"}
+        facePosition={[50, 0]}
+      />
+      <Character
+        type={"rectangle"}
+        height={col1.height * 0.2}
+        placement={[col1.x + col1.width * 0.1, col1.y + col1.height * 0.35]}
+        color={blue}
+        mood={"neutral"}
+        facePosition={[70, 50]}
+      />
+      <Character
+        type={"scaleneTriangle"}
+        height={col1.height * 0.2}
+        placement={[col1.x + col1.width * 0.8, col1.y + col1.height * 0.55]}
+        color={green}
+        mood={"happy"}
+        facePosition={[5, 100]}
+      />
+      <Character
+        type={"trapezoid"}
+        height={col1.height * 0.15}
+        placement={[col1.x + col1.width * 0.95, col1.y + col1.height * 0.65]}
+        color={pink}
+        mood={"neutral"}
+        facePosition={[70, 50]}
+      />
+      <Character
+        type={"square"}
+        height={col1.height * 0.3}
+        placement={[col1.x + col1.width * 1.25, col1.y + col1.height * 0.75]}
+        color={green}
+        mood={"happy"}
+        facePosition={[70, 50]}
+      />
     </Stage>
   );
 };
