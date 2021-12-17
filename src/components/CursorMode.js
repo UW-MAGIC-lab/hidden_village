b8fcfd3e2f365e5a476a1dc50f53921807e59514
Ariel Fogel <fogeltine@gmail.com>
2021-12-17 09:44:23 -0600

    update cursormode to function using a statemachine

--- src/components/CursorMode.js
+++ src/components/CursorMode.js
@@ -1,32 +1,54 @@
 import { Rectangle } from "@pixi/math";
 import { Container, Sprite } from "@inlet/react-pixi";
 import { useState, useEffect, useRef } from "react";
+import CursorMachine from "../machines/cursorMachine";
+import { useMachine, useSelector } from "@xstate/react";
 
-const hitAreasIntersect = (a, b) => {
+const hitAreasIntersect = (hitAreaOne, hitAreaTwo) => {
   return (
-    a.x < b.x + b.width &&
-    a.x + a.width > b.x &&
-    a.y < b.y + b.height &&
-    a.y + a.height > b.y
+    hitAreaOne.x < hitAreaTwo.x + hitAreaTwo.width &&
+    hitAreaOne.x + hitAreaOne.width > hitAreaTwo.x &&
+    hitAreaOne.y < hitAreaTwo.y + hitAreaTwo.height &&
+    hitAreaOne.y + hitAreaOne.height > hitAreaTwo.y
   );
 };
+
+const selectHovering = (state) => state.context.hovering;
+
+/**
+ * @name CursorMode
+ * @param {Function} callback what will fire when you hover over the next button with the cursor
+ * @param {Function} rowDimensionsCallback layoutFunction to provide position data
+ * @param {Object} poseData poseData necessary for controlling the cursor
+ *
+ * @component
+ * @example
+ *
+ *
+ */
 
 const CursorMode = (props) => {
   const nextButtonRef = useRef(null);
   const cursorRef = useRef(null);
-  const [hovering, setHovering] = useState(false);
+  const [state, send, service] = useMachine(CursorMachine, {
+    context: { callback: props.callback },
+  });
+  const hovering = useSelector(service, selectHovering);
+  const [rowDimensions] = useState(props.rowDimensions(2));
+
   const [cursor] = useState(new URL("../assets/cursor.png", import.meta.url));
   const [nextButton, setNextButton] = useState(
     new URL("../assets/next_button.png", import.meta.url)
   );
   const [cursorCoordinates, setCursorCoordinates] = useState({
-    x: window.innerWidth * 0.5,
+    x: window.innerWidth * 0.65,
     y: window.innerHeight * 0.5,
   });
   const [nextButtonCoordinates] = useState({
-    x: window.innerWidth * 0.9,
+    x: rowDimensions.width - 3 * rowDimensions.margin,
     y: window.innerHeight * 0.85,
   });
+
   useEffect(() => {
     if (hovering) {
       setNextButton(
@@ -43,7 +65,7 @@ const CursorMode = (props) => {
       props.poseData.poseLandmarks[15] !== undefined
     ) {
       setCursorCoordinates({
-        x: window.innerWidth * (props.poseData.poseLandmarks[15].x + 0.15),
+        x: window.innerWidth * (props.poseData.poseLandmarks[15].x + 0.2),
         y: window.innerHeight * props.poseData.poseLandmarks[15].y + 0.1,
       });
       if (
@@ -52,11 +74,7 @@ const CursorMode = (props) => {
           nextButtonRef.current.hitArea
         )
       ) {
-        setHovering(true);
-        setTimeout(() => {
-          props.callback();
-          setHovering(false);
-        }, 1000);
+        send("TRIGGER");
       }
     }
   }, [props.poseData]);
@@ -67,8 +85,6 @@ const CursorMode = (props) => {
         image={nextButton.href}
         x={nextButtonCoordinates.x}
         y={nextButtonCoordinates.y}
-        mouseover={() => setHovering(true)}
-        mouseout={() => setHovering(false)}
         interactive
         anchor={0}
         ref={nextButtonRef}
