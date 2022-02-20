5a342a55e626f2711bf52dfe32f488485784d18a
Ariel Fogel <fogeltine@gmail.com>
2022-02-20 13:55:52 -0600

    update Video for formatting

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,25 +1,34 @@
-import { PixiComponent } from '@inlet/react-pixi';
+import { PixiComponent } from "@inlet/react-pixi";
 
-const Video = PixiComponent('Video', {
-  create: props => {
+const Video = PixiComponent("Video", {
+  create: (props) => {
     const { path, mp4path, onComplete, x, y } = props;
-    
+
     // these three lines are not functioning as expected
-    const videoPath = new URL(`${path}`, import.meta.url)
-    const videoPathNoTemplateString = new URL(path, import.meta.url)
-    const mp4VideoPath = new URL(mp4path, import.meta.url)
-    let ultimatePath = new URL("../assets/animations/oppose_angle.mp4", import.meta.url);
-    const otherUrl = new URL("../assets/animations/other_one.webm", import.meta.url)
+    const videoPath = new URL(`${path}`, import.meta.url);
+    const videoPathNoTemplateString = new URL(path, import.meta.url);
+    const mp4VideoPath = new URL(mp4path, import.meta.url);
+    let ultimatePath = new URL(
+      "../assets/animations/oppose_angle.mp4",
+      import.meta.url
+    );
+    const otherUrl = new URL(
+      "../assets/animations/other_one.webm",
+      import.meta.url
+    );
     // these two lines are functioning as expected
     switch (path) {
-      case '../assets/animations/other_one.webm':
+      case "../assets/animations/other_one.webm":
         ultimatePath = otherUrl;
         break;
-      case '../assets/animations/oppose_angle.mp4':
-        ultimatePath = new URL("../assets/animations/oppose_angle.mp4", import.meta.url)
+      case "../assets/animations/oppose_angle.mp4":
+        ultimatePath = new URL(
+          "../assets/animations/oppose_angle.mp4",
+          import.meta.url
+        );
         break;
       default:
-        ultimatePath = new URL(path, import.meta.url)
+        ultimatePath = new URL(path, import.meta.url);
         break;
     }
     // const otherMp4Url = new URL("../assets/animations/oppose_angle.mp4", import.meta.url)
@@ -28,7 +37,7 @@ const Video = PixiComponent('Video', {
     video.x = x;
     video.y = y;
     const videoController = video.texture.baseTexture.resource.source;
-    videoController.addEventListener('ended', onComplete);
+    videoController.addEventListener("ended", onComplete);
     // instantiate something and return it.
     // for instance:
     return video;
@@ -52,7 +61,7 @@ const Video = PixiComponent('Video', {
     // default true
     destroyChildren: true,
   },
-})
+});
 
 // const  = forwardRef((props, ref) => (
 //   <PixiVideoComponent ref={ref} app={useApp()} {...props} />
