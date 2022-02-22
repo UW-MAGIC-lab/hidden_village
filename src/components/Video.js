8b8aa7e13858d5d2e2354881da2a15bc4d401b90
Ariel Fogel <fogeltine@gmail.com>
2022-02-22 13:42:35 -0600

    first step to adding standalone experiment component

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,23 +2,22 @@ import { PixiComponent } from "@inlet/react-pixi";
 
 const Video = PixiComponent("Video", {
   create: (props) => {
-    const { path, mp4path, onComplete, x, y } = props;
+    const { path, mp4path, onComplete, x, y, width, height } = props;
 
-    // these three lines are not functioning as expected
-    const videoPath = new URL(`${path}`, import.meta.url);
-    const videoPathNoTemplateString = new URL(path, import.meta.url);
-    const mp4VideoPath = new URL(mp4path, import.meta.url);
+    // HACK: as mentioned in the parcel issue https://github.com/parcel-bundler/parcel/issues/7643
+    // there's a weirdness that still exists with how parcel is resolving
+    // the URL paths
     let ultimatePath = new URL(
       "../assets/animations/oppose_angle.mp4",
       import.meta.url
     );
     const otherUrl = new URL(
-      "../assets/animations/other_one.webm",
+      "../assets/animations/opposite_angle.webm",
       import.meta.url
     );
     // these two lines are functioning as expected
     switch (path) {
-      case "../assets/animations/other_one.webm":
+      case "../assets/animations/opposite_angle.webm":
         ultimatePath = otherUrl;
         break;
       case "../assets/animations/oppose_angle.mp4":
@@ -31,41 +30,20 @@ const Video = PixiComponent("Video", {
         ultimatePath = new URL(path, import.meta.url);
         break;
     }
-    // const otherMp4Url = new URL("../assets/animations/oppose_angle.mp4", import.meta.url)
     const texture = PIXI.Texture.from(ultimatePath.href);
     const video = new PIXI.Sprite(texture);
     video.x = x;
     video.y = y;
+    video.width = width;
+    video.height = height;
     const videoController = video.texture.baseTexture.resource.source;
     videoController.addEventListener("ended", onComplete);
-    // instantiate something and return it.
-    // for instance:
     return video;
   },
-  // didMount: (instance, parent) => {
-  //   // apply custom logic on mount
-  // },
-  // willUnmount: (instance, parent) => {
-  //   // clean up before removal
-  // },
-  // applyProps: (instance, oldProps, newProps) => {
-  //   // props changed
-  //   // apply logic to the instance
-  // },
   config: {
-    // destroy instance on unmount?
-    // default true
     destroy: true,
-
-    /// destroy its children on unmount?
-    // default true
     destroyChildren: true,
   },
 });
 
-// const  = forwardRef((props, ref) => (
-//   <PixiVideoComponent ref={ref} app={useApp()} {...props} />
-// ));
-
-// console.log(Video);
 export default Video;
