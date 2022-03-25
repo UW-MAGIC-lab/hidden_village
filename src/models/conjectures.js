84b773d1e7864aecfc0779ae869289aff9a5e7e0
Ariel Fogel <fogeltine@gmail.com>
2022-03-25 17:04:12 -0500

    add in two additional poses, extract rawpose data to conjectures module

--- src/models/conjectures.js
+++ src/models/conjectures.js
@@ -43,6 +43,11 @@
  * @param {ExperimentConjecture} props.conjectureData
  */
 
+ import oppositeAnglePoseData from "./rawPoses/oppositeAnglePoses.json";
+ import parallelogramPoseData from "./rawPoses/area_parallelogram.json";
+ import areaDoubled from './rawPoses/area_doubled.json';
+ import angleAngleAngle from './rawPoses/angleAngleAnglePoses.json';
+
 const conjectures = [
   {
     conjecture:
@@ -56,18 +61,37 @@ const conjectures = [
     poseDataFileName: "area_parallelogram.json",
     videoPath: "../assets/animations/area_parallelogram_trimmed.webm",
   },
-  // {
-  //   conjecture:
-  //     "If you double the length and the width of a rectangle, then the area is exactly doubled.",
-  //   poseDataFileName: "oppositeAnglePoses.json",
-  //   videoPath: "../assets/animations/area_doubled.webm",
-  // },
-  // {
-  //   conjecture:
-  //     "Given that you know the measure of all three angles of a triangle, there is only one unique triangle that can be formed with these three angle measurements.",
-  //   poseDataFileName: "oppositeAnglePoses.json",
-  //   videoPath: "../assets/animations/angle_angle_angle.webm",
-  // },
+  {
+    conjecture:
+      "If you double the length and the width of a rectangle, then the area is exactly doubled.",
+    poseDataFileName: "area_doubled.json",
+    videoPath: "../assets/animations/area_doubled_trimmed.webm",
+  },
+  {
+    conjecture:
+      "Given that you know the measure of all three angles of a triangle, there is only one unique triangle that can be formed with these three angle measurements.",
+    poseDataFileName: "angleAngleAnglePoses.json",
+    videoPath: "../assets/animations/angle_angle_angle_trimmed.webm",
+  },
 ];
 
-export { conjectures };
+const getPoseData = (poseDataFileName) => {
+  switch (poseDataFileName) {
+    case "oppositeAnglePoses.json":
+      return oppositeAnglePoseData;
+      break;
+    case "area_parallelogram.json":
+      return parallelogramPoseData;
+      break;
+    case "area_doubled.json":
+      return areaDoubled;
+      break;
+    case "angleAngleAnglePoses.json":
+      return angleAngleAngle;
+      break;
+    default:
+      return null;
+  }
+}
+
+export { conjectures, getPoseData };
