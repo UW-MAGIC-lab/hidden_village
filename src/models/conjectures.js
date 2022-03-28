28cf80f3368f766a606bbe532f8faf3de36073fb
Ariel Fogel <fogeltine@gmail.com>
2022-03-28 13:55:41 -0500

    add all 8 conjectures - extracts conjecture info for videos into conjectures.js - removes prototype language from the script

--- src/models/conjectures.js
+++ src/models/conjectures.js
@@ -69,18 +69,39 @@ const conjectures = [
     videoPath: "../assets/animations/angle_angle_angle_trimmed.webm",
   },
   {
-    conjecture:
-      "The diagonals of a rectangle always have the same length",
+    conjecture: "The diagonals of a rectangle always have the same length",
     poseDataFileName: "diagonals_rectangle.json",
     videoPath: "../assets/animations/diagonals_rectangle.webm",
   },
+  {
+    conjecture:
+      "If one angle of a triangle is larger than the second angle, then the side opposite first angle is longer than the side opposite the second angle",
+    poseDataFileName: "side_angle_side_poses.json",
+    videoPath: "../assets/animations/side_angle_side_trimmed.webm",
+  },
+  {
+    conjecture:
+      "The sum of the length of two sides of a triangle is always greater than the length of the third side",
+    poseDataFileName: "sum_two_sides_poses.json",
+    videoPath: "../assets/animations/sum_two_sides_trimmed.webm",
+  },
+  {
+    conjecture:
+      "Reflecting any point over the x-axis is the same as rotating the point 90 degrees clockwise about the origin",
+    poseDataFileName: "reflection_over_axis_poses.json",
+    videoPath: "../assets/animations/reflection_over_axis_trimmed.webm",
+  },
 ];
+
 // Import the pose data
 import oppositeAnglePoseData from "./rawPoses/oppositeAnglePoses.json";
 import parallelogramPoseData from "./rawPoses/area_parallelogram.json";
-import areaDoubled from './rawPoses/area_doubled.json';
-import angleAngleAngle from './rawPoses/angleAngleAnglePoses.json';
-import diagonalsRectangle from './rawPoses/diagonals_rectangle.json';
+import areaDoubled from "./rawPoses/area_doubled.json";
+import angleAngleAngle from "./rawPoses/angleAngleAnglePoses.json";
+import diagonalsRectangle from "./rawPoses/diagonals_rectangle.json";
+import reflectionOverAxis from "./rawPoses/reflection_over_axis_poses.json";
+import sumTwoSides from "./rawPoses/sum_two_sides_poses.json";
+import sideAngleSide from "./rawPoses/side_angle_side_poses.json";
 // reference the pose data
 const getPoseData = (poseDataFileName) => {
   switch (poseDataFileName) {
@@ -99,9 +120,82 @@ const getPoseData = (poseDataFileName) => {
     case "diagonals_rectangle.json":
       return diagonalsRectangle;
       break;
+    case "reflection_over_axis_poses.json":
+      return reflectionOverAxis;
+      break;
+    case "sum_two_sides_poses.json":
+      return sumTwoSides;
+      break;
+    case "side_angle_side_poses.json":
+      return sideAngleSide;
+      break;
     default:
       return null;
   }
-}
+};
 
-export { conjectures, getPoseData };
+// ***********************************
+// Conjecture Video Data
+// ***********************************
+// HACK: as mentioned in the parcel issue https://github.com/parcel-bundler/parcel/issues/7643
+// there's a weirdness that still exists with how parcel is resolving
+// the URL paths
+
+const oppositeAnglePath = new URL(
+  "../assets/animations/opposite_angle.webm",
+  import.meta.url
+);
+const parallelogramPath = new URL(
+  "../assets/animations/area_parallelogram_trimmed.webm",
+  import.meta.url
+);
+const angleAngleAnglePath = new URL(
+  "../assets/animations/angle_angle_angle_trimmed.webm",
+  import.meta.url
+);
+const areaDoubledPath = new URL(
+  "../assets/animations/area_doubled_trimmed.webm",
+  import.meta.url
+);
+const diagonalsRectanglePath = new URL(
+  "../assets/animations/diagonals_rectangle.webm",
+  import.meta.url
+);
+const reflectionOverAxisPath = new URL(
+  "../assets/animations/reflection_over_axis_trimmed.webm",
+  import.meta.url
+);
+const sideAngleSidePath = new URL(
+  "../assets/animations/side_angle_side_trimmed.webm",
+  import.meta.url
+);
+const sumTwoSidesPath = new URL(
+  "../assets/animations/sum_two_sides_trimmed.webm",
+  import.meta.url
+);
+
+const getVideoFromPath = (path) => {
+  // default is opposite angle
+  switch (path) {
+    case "../assets/animations/opposite_angle.webm":
+      return oppositeAnglePath;
+    case "../assets/animations/area_parallelogram_trimmed.webm":
+      return parallelogramPath;
+    case "../assets/animations/angle_angle_angle_trimmed.webm":
+      return angleAngleAnglePath;
+    case "../assets/animations/area_doubled_trimmed.webm":
+      return areaDoubledPath;
+    case "../assets/animations/diagonals_rectangle.webm":
+      return diagonalsRectanglePath;
+    case "../assets/animations/reflection_over_axis_trimmed.webm":
+      return reflectionOverAxisPath;
+    case "../assets/animations/side_angle_side_trimmed.webm":
+      return sideAngleSidePath;
+    case "../assets/animations/sum_two_sides_trimmed.webm":
+      return sumTwoSidesPath;
+    default:
+      return new URL(path, import.meta.url);
+  }
+};
+
+export { conjectures, getPoseData, getVideoFromPath };
