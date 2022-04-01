110e46605fb35989140fe817524f079cb1787744
tyrrr110 <tren25@wisc.edu>
2022-03-31 20:19:40 -0500

    simple timer implemented

--- src/components/CapturePose.js
+++ src/components/CapturePose.js
@@ -1,23 +1,40 @@
 import { useEffect, useState } from "react";
+import { flatten } from "xstate/lib/utils";
 import EditorCanvas from "./EditorCanvas";
 
 const CapturePose = (props) => {
   const {poseData} =  props;
   const [open, setOpen] = useState(false);
   const [isCountingDown, setIsCountingDown] = useState(false);
+  const [showCounter, setShowCounter] = useState(false);
+  const [counter, setCounter] = useState(0);
   const [hasCapturedPose, setHasCapturedPose] = useState(false);
-  const [capturedPoses, setCapturedPoses] = useState({}); // object(hash-table): key-value pair
+  const [capturedPose, setCapturedPose] = useState({}); // object(hash-table): key-value pair
 
-  const tryCapture = () => {
+  const tryCapture = (event) => {
     if (!isCountingDown) {
+      let countdown = 3;
+      setCounter(countdown);
+      let button = event.currentTarget;
       setIsCountingDown(true);
-      console.log("Capturing!");
+      setShowCounter(true);
       // timer starts counting down
-      console.log("3! 2! 1!");
-      console.log("Captured!");
-      setCapturedPoses(poseData);
-      setHasCapturedPose(true);
-      setIsCountingDown(false);
+      const timerId = setInterval((event) => {
+        button.interactive = false;
+        button.buttonMode = false;
+        console.log(countdown);
+        setCounter(--countdown);
+        // finish capturing
+        if (countdown < 0) {
+          clearInterval(timerId);
+          button.interactive = true;
+          button.buttonMode = true;
+          setCapturedPose(poseData);
+          setHasCapturedPose(true);
+          setIsCountingDown(false);
+          setShowCounter(false);
+        }
+      }, 1000);
     }
   };
 
@@ -35,44 +52,52 @@ const CapturePose = (props) => {
         {/* real-time camera canvas */}
         <div
           className="
-            grid grid-cols-1 gap-3 justify-items-center place-content-around"
+            grid grid-cols-1 gap-3 justify-items-center place-content-center"
         >
           <EditorCanvas width={400} height={300} poseData={poseData} />
+          {showCounter && <div className="text-8xl z-10">{counter}</div>}
           <button 
             className="
             bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded
             justify-self-center"
-            onClick={() => { if (!isCountingDown) tryCapture()}}
+            onClick={(event) => { if (!isCountingDown) tryCapture(event)}}
           >
             Capture
           </button>
-          {/* {open && (
-            <div
-              className="
-                "
-            >
-            </div>
-          )}
-          {!open && (
-            <div>CLOSE STATE</div>
-          )} */}
         </div>
         {/* captured pose preview canvas */}
         <div
           className="
-            grid grid-cols-1 gap-3 justify-items-center place-content-around"
+            grid grid-cols-2 gap-3 justify-items-center place-content-center"
         >
           {hasCapturedPose &&
-            <EditorCanvas width={400} height={300} poseData={capturedPoses} />
+            <div className="col-start-1 col-span-2">
+              <EditorCanvas width={400} height={300} poseData={capturedPose} />
+            </div>
           }
           {/* {hasCapturedPose &&
             (console.log(capturedPoses[0]))
           } */}
+          {/* buttons at the bottom */}
           <button 
             className="
             bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded
-            justify-self-end"
-            onClick={() => setOpen(!open)}
+            justify-self-center"
+            onClick={() => {
+              // TODO: save captured pose data
+              setOpen(!open);
+            }}
+          >
+            Save
+          </button>
+          <button 
+            className="
+            bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded
+            justify-self-center"
+            onClick={() => {
+              setHasCapturedPose(false); // don't save pose
+              setOpen(!open);
+            }}
           >
             Cancel
           </button>
