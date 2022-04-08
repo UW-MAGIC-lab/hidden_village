c9dc393352924df6a85a27b1ff8642061ff3affa
Ariel Fogel <fogeltine@gmail.com>
2022-04-08 15:23:54 -0500

    update copy for video instruction

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -39,7 +39,7 @@ const VideoPlayer = (props) => {
     <>
       <Graphics draw={drawModalBackground} />
       <Text
-        text={"Watch this video,\nthen match the movements!"}
+        text={"Watch this video. \nAfter it finishes, you'll be asked to match the movements!"}
         x={columnDimensions(2).x + columnDimensions(2).margin}
         y={columnDimensions(2).height / 2}
         style={
