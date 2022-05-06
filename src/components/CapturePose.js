cc7977b435252b0db5887632c6b81f4a40da98b4
tyrrr110 <tren25@wisc.edu>
2022-05-06 10:16:08 -0500

    default behavior as save, and only close button and capture button for capPose

--- src/components/CapturePose.js
+++ src/components/CapturePose.js
@@ -6,12 +6,9 @@ const CapturePose = (props) => {
   const { poseData, index, onDelete, onGetPoseData } = props;
   const [open, setOpen] = useState(false);
   const [isCountingDown, setIsCountingDown] = useState(false);
-  const [showCounter, setShowCounter] = useState(false);
   const [counter, setCounter] = useState(0);
-  const [hasCaptured, setHasCaptured] = useState(false);
   const [hasSavedPose, setHasSavePose] = useState(false);
   const [capturedPose, setCapturedPose] = useState({}); // object(hash-table): key-value pair
-  const [prevPose, setPrevPose] = useState({}); // object(hash-table): key-value pair
 
   const tryCapture = (event) => {
     if (!isCountingDown) {
@@ -19,33 +16,31 @@ const CapturePose = (props) => {
       setCounter(countdown);
       let button = event.currentTarget;
       setIsCountingDown(true);
-      setShowCounter(true);
       // timer starts counting down
-      const timerId = setInterval((event) => {
+      const timerId = setInterval(() => {
         button.interactive = false;
         button.buttonMode = false;
         setCounter(--countdown);
         // finish capturing
         if (countdown <= 0) {
           clearInterval(timerId);
-          button.interactive = true;
-          button.buttonMode = true;
-
-          setPrevPose(capturedPose);
           setCapturedPose(poseData);
 
-          setHasSavePose(true);
-          setHasCaptured(true);
-
+          button.interactive = true;
+          button.buttonMode = true;
           setIsCountingDown(false);
-          setShowCounter(false);
+          setHasSavePose(true);
         }
       }, 1000);
     }
   };
 
+  useEffect(() => {
+    onGetPoseData(index, capturedPose);
+  }, [capturedPose]);
+
   return (
-    <div>
+    <div className="relative">
       <div className="font-medium text-lg justify-self-start px-4 py-2">
         <PoseName defaultName={`My Pose ${index}`} index={index}></PoseName>
       </div>
@@ -53,19 +48,21 @@ const CapturePose = (props) => {
       {open && (
         <div
           className="
-        grid grid-cols-2 gap-3 justify-items-center place-content-center"
+          grid grid-cols-2 gap-3 m-3 justify-items-center place-content-center"
         >
           {/* real-time camera canvas */}
           <div
             className="
-          grid grid-cols-1 gap-3 justify-items-center place-content-center"
+            grid grid-cols-1 gap-3 justify-items-center place-content-center relative"
           >
-            <EditorCanvas width={300} height={200} poseData={poseData} />
-            {showCounter && <div className="text-8xl z-10">{counter}</div>}
+            <EditorCanvas width={280} height={260} poseData={poseData} />
+            <div className="absolute bottom-12">
+              {isCountingDown && <div className="text-slate-500 text-8xl z-10 p-6">{counter}</div>}
+            </div>
             <button
               className="
-            bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded
-            justify-self-center"
+              bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded
+              justify-self-center"
               onClick={(event) => {
                 if (!isCountingDown) tryCapture(event);
               }}
@@ -76,43 +73,21 @@ const CapturePose = (props) => {
           {/* captured pose preview canvas */}
           <div
             className="
-            grid grid-cols-2 gap-3 justify-items-center place-content-center"
+            grid grid-cols-1 gap-3 justify-items-center place-content-center"
           >
             {hasSavedPose && (
-              <div className="col-start-1 col-span-2">
-                <EditorCanvas
-                  width={300}
-                  height={200}
-                  poseData={capturedPose}
-                />
-              </div>
+              <EditorCanvas width={280} height={260} poseData={capturedPose}/>
             )}
-            {/* buttons at the bottom */}
+            {/* button at the bottom */}
             <button
               className="
             bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded
             justify-self-center"
               onClick={() => {
-                setHasCaptured(false);
-                onGetPoseData(index, capturedPose);
                 setOpen(!open);
               }}
             >
-              Save
-            </button>
-            <button
-              className="
-            bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded
-            justify-self-center"
-              onClick={() => {
-                if (hasCaptured) {
-                  setCapturedPose(prevPose); // don't save pose
-                }
-                setHasCaptured(false);
-                setOpen(!open);
-              }}
-            >
-              Cancel
+              Close
             </button>
           </div>
         </div>
@@ -134,6 +109,12 @@ const CapturePose = (props) => {
           >
             Delete
           </button>
+          {/* Saved Pose Preview */}
+          {hasSavedPose && (
+            <div className="col-start-3 col-span-4 justify-self-end absolute bottom-0">
+              <EditorCanvas width={100} height={80} poseData={capturedPose}/>
+            </div>
+          )}
         </div>
       )}
     </div>
