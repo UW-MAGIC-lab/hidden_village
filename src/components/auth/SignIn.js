412ae35fa3a5ffb3b4ca666f64adb4215f949d56
ctstewart <8145799+ctstewart@users.noreply.github.com>
2023-02-28 21:53:00 -0600

    Styled the login page Removed functionality for now, will be added back in

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,6 +1,8 @@
+import "../utilities/Loader.css";
+import "./SignIn.css";
+import loginWithChar from "../../assets/LOGIN_wCHAR_666.png";
 // Import FirebaseAuth and firebase.
 import React from "react";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
@@ -16,10 +18,38 @@ const uiConfig = {
 const SignInScreen = () => {
   return (
     <div>
-      {/* <h1>My App</h1> */}
-      {/* <p>Please sign-in:</p> */}
-      <br />
-      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+      <div className="model-loader">
+        <div className="login">
+          <img src={loginWithChar} />
+          <div className="login-inputs">
+            <label className="login-inputs-label">Login</label>
+            <input className="login-inputs-input" type="text" />
+            <br />
+            <br />
+            <label className="login-inputs-label">Password</label>
+            <input className="login-inputs-input" type="password" />
+          </div>
+        </div>
+        <br />
+        <div className="mosaic-loader">
+          <div className="cell d-0"></div>
+          <div className="cell d-1"></div>
+          <div className="cell d-2"></div>
+          <div className="cell d-3"></div>
+          <div className="cell d-1"></div>
+          <div className="cell d-2"></div>
+          <div className="cell d-3"></div>
+          <div className="cell d-4"></div>
+          <div className="cell d-2"></div>
+          <div className="cell d-3"></div>
+          <div className="cell d-4"></div>
+          <div className="cell d-5"></div>
+          <div className="cell d-3"></div>
+          <div className="cell d-4"></div>
+          <div className="cell d-5"></div>
+          <div className="cell d-6"></div>
+        </div>
+      </div>
     </div>
   );
 };
