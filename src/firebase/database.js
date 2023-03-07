98a9ab4dbae4bf02d20dbf7a601fb7a999a4afc9
Jeetbhakta <112580620+JeetBhakta@users.noreply.github.com>
2023-03-07 11:53:39 -0600

    Write functionality
    Added some basic writing functionality in database.js, along with more details on the format to which we will be storing the data in app.js

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -1,2 +1,10 @@
 // Firebase Init
-import { initializeApp } from "firebase/database";
+import { getDatabase, ref, set, onValue } from "firebase/database";
+
+export const db = getDatabase();
+
+export function writeUserData(poseData) {
+  set(ref(db, "/experimentalData"), {
+    Pose_Data: poseData,
+  });
+}
